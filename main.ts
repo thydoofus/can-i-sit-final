7e2004340fc6435c9428e9d809a44bf62fafe7a9
thydoofus <77323809+thydoofus@users.noreply.github.com>
2022-11-04 02:13:14 +0800

    change previous normal config to spam keys while mapping OBS studio

--- main.ts
+++ main.ts
@@ -1,12 +1,19 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Yes)
+    keyboard.sendString("v")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    keyboard.sendString("x")
 })
 keyboard.startKeyboardService()
 basic.forever(function () {
-    if (input.pinIsPressed(TouchPin.P0) || input.buttonIsPressed(Button.A)) {
+    control.waitMicros(500)
+    if (pins.analogReadPin(AnalogPin.P0) >= 900) {
         basic.showIcon(IconNames.Yes)
         keyboard.sendString("v")
     } else {
         basic.showIcon(IconNames.No)
+        keyboard.sendString("x")
     }
 })
